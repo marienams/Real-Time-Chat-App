10c8e6bf789f8004fc97e02a80bb10e83f578d26
Mahima Singh <mahima@Mahimas-MacBook-Pro.local>
2025-01-11 00:31:50 +0530

    added initial files, created a basic folder structure, successfully connected to mongoDB, routes not yet working

---
+++ Backend/src/lib/db.lib.js
@@ -0,0 +1,13 @@
+// CONNECT TO MONGODB
+
+import mongoose from "mongoose"
+
+export const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    }
+    catch {
+        console.log("MongoDB not connected: ", error)
+    }
+}
