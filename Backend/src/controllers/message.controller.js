2630d0623e8ca616c9bb5511281a2dbfb7bcf513
Mahima Singh <mahima@Mahimas-MacBook-Pro.local>
2025-01-11 22:40:28 +0530

    added a message db model, routes for getting all the users, route for sending messages and getting message history

---
+++ Backend/src/controllers/message.controller.js
@@ -0,0 +1,78 @@
+import User from "../models/user.model.js"
+import Message from "../models/message.model.js"
+import cloudinary from "../lib/cloud_config.js";
+
+// getting all the users in the chat list
+export const getUsersForSideBar = async(req,res) =>{
+    // display all users except self in sidebar
+    try{
+        const loggedInUserId = req.user._id;
+        // find all users except the current logged in user
+        const filteredUsers = await User.find({_id: {$ne:loggedInUserId}}).select("-password")
+
+        res.status(200).json(filteredUsers)
+    }
+    catch(error){
+        console.log("Error in message controller ", error.message)
+        res.status(500).json({error:"Internal server error"})
+    }
+}
+
+// getting messages from 2 users (me and other)
+export const getMessages = async (req, res) => {
+    try{
+        // getting Id of receiver
+        const{id:userToChatId} = req.params
+        const myId = req.user._id
+
+        const messages = await Message.find({
+            // finding all the messages sent and received and vice versa conditions
+            $or: [
+                {senderId: myId, receiverId: userToChatId},
+                {receiverId: userToChatId, senderId: myId}
+            ]
+        })
+
+        res.status(200).json(messages)
+    }
+    catch(error){
+        console.log("Error at message controller in getMessages ", error.message)
+        res.status(500).json({error: "Internal server error"})
+    }
+}
+
+export const sendMessage =  async (req,res) => {
+    try{
+        // grab text and or image sent, receiver and sender
+        const {text, image} = req.body
+        const {id: receiverId} = req.params
+        const senderId = req.user._id
+        // when user uploads an image
+        let imageUrl;
+
+        if(image){
+            // upload base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url
+        }
+
+        // now we add the new message to db
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        })
+
+        await newMessage.save()
+
+        // todo: relatime messaging using socket.io
+
+        res.status(201).json(newMessage)
+    }
+    catch(error){
+        console.log("Error at message controller ", error.message)
+        res.status(500).json({error: "Internal server error"})
+    }
+}
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.routes.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.lib.js"
 import cookieParser from "cookie-parser"
@@ -12,6 +13,7 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, ()=>{
     console.log("Server is running on ", PORT)
