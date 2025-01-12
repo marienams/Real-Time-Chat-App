6d4eec42c999fa3b45cfb931b0b740343e8edae1
Mahima Singh <mahima@Mahimas-MacBook-Pro.local>
2025-01-12 23:11:31 +0530

    project completed  deploy ready

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,10 +7,13 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import {app, server} from"./lib/socket.js"
 
+import path from "path"
+
 dotenv.config()
 
 // we stored our server port in a dotenv file
 const PORT = process.env.PORT
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -21,6 +24,15 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+// serving frontend app
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"))
+    })
+}
 
 server.listen(PORT, ()=>{
     console.log("Server is running on ", PORT)
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    // dynamic url
+    baseURL: import.meta.env.MODE === "development"? "http://localhost:5001/api":"/api",
     withCredentials: true, // send cookies in every request
 })
